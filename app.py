6a799a7eb00c4db1d0c20a1f51f7857d2e2e7e5b
yash2learn <y.sharma.nagarajarao@gmail.com>
2022-06-12 14:39:06 +0530

    updated the title

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def home():
-    return "Machine learning project"
+    return "<h>Updated title for Machine learning project to test CI CD pipeline</h>"
 
 if __name__=="__main__":
     app.run(debug=True)
